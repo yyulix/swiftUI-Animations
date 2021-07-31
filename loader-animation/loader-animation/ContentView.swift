47f653009f2da49b0e66d5b267c17bb3b2b71912
Yulia <yuliarandomchenko@gmail.com>
2021-07-31 17:38:37 +0300

    Add Loader animation

---
+++ loader-animation/loader-animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  loader-animation
+//
+//  Created by Y u l i a on 31.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ loader-animation/loader-animation/loader_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  loader_animationApp.swift
+//  loader-animation
+//
+//  Created by Y u l i a on 31.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct loader_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
